ca6648e47dde5b9289354b1c2c9db3cc5bbe7b8c
Sebastian Rotar <rotar.seby1@gmail.com>
2019-02-19 15:03:17 +0100

    task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ArraySortPipe } from "./sort.pipe";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  providers: [ArraySortPipe]
 })
 export class AppComponent {
-  title = 'ang-assignment';
+  constructor(private sortPipe: ArraySortPipe) {}
+
+  dish: {} = {
+    name: "Uthapizza",
+    image:
+      "https://thestingyvegan.com/wp-content/uploads/2017/10/vegan-nacho-pizza-photo.jpg",
+    category: "mains",
+    label: "Hot",
+    price: "4.99",
+    description:
+      "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+      }
+    ]
+  };
+
+  sortBy(form: NgForm) {
+    const value = form.value;
+    const array = this.dish["comments"];
+    this.sortPipe.transform(array, value.choice);
+  }
 }
